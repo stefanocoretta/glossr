24b48447233f751d1c9981d700676ee0cf0e98c6
stefanocoretta <stefano.coretta@gmail.com>
2020-12-29 08:19:56 +0100

    ✨ - refactor read_sentence()

--- R/read.R
+++ R/read.R
@@ -1,10 +1,11 @@
-#' Read sentences
+#' Read sentence
+#'
+#' Read sentence file from specified path.
+#'
+#' @param path The path to the text object as a string.
+#' @param sentence The name of the sentence object as a string.
 #'
 #' @export
-read_sentences <- function(..., sentence, db = ".") {
-  path <- stringr::str_flatten(c(db, unlist(list(...))), collapse = "/")
-  sent_path <- paste0(path, "/", sentence, "_sent.yaml")
-  yaml::read_yaml(sent_path)
+read_sentence <- function(path, sentence) {
+  yaml::read_yaml(paste0(path, "/", sentence, "_sent.yaml" ))
 }
-
-
